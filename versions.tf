d2baf5b93cd365c13affbfdc1796350969531cc7
Sergey Andryunin <s.andryunin@Sergejs-MacBook-Pro.local>
2021-12-20 08:03:23 +0300

    fix versions && syntax

--- versions.tf
+++ versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "~> 0.44.0"
+      version = "~> 0.61.0"
     }
   }
-  required_version = ">= 0.13"
+  required_version = ">= 1.1.0"
 }
