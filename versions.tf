0a0f6856ed392e37d1fed26f804f321e115b5b28
ra-leonid <96412355+ra-leonid@users.noreply.github.com>
2023-03-25 23:02:12 +0300

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "~> 0.88.0"
+      version = ">= 0.61.0"
     }
   }
-  required_version = ">= 1.1.0"
+  # required_version = ">= 1.1.0"
 }
